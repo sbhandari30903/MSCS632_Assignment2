f9aa1bb770941699323de75b1574bf0bb0563b1e
sbhandari30903 <sbhandari30903@ucumberlands.edu>
2025-02-01 16:26:52 -0500

    Added Python, C++ and Rust code that shows how memory is handled.

--- MemoryManagement/mem_manage.rs
+++ MemoryManagement/mem_manage.rs
@@ -1,6 +1,6 @@
 fn main() {
     let s = String::from("allocate memory"); // allocate memory in heap
-    print_string(&s); // borrowing s
+    print_string(&s); // borrowing s and now the mem of string belongs to the function. s does not have anything
     // do not need to explicetly dellocate mem. Rust's ownership handles it
 }
 
